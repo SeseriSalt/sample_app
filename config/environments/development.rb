ca93daa3d35290a3e4ae5d05aff994d6c4cc75ef
SeseriSalt <sho8.kthy@gmail.com>
2022-05-26 09:18:25 +0000

    add accountactivation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = '3fa1e64448b24b40a4f5cdc486aa8cd5.vfs.cloud9.us-east-2.amazonaws.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,6 +4,10 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+  
   test "invaild signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -12,16 +16,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
-    assert_select 'div.alert'
+    assert_select 'div.field_with_errors'
   end
   
   
-  test "vaild signup information" do
+  test "vaild signup information with account activation" do
     get signup_path
     assert_difference "User.count", 1 do
       post users_path, params: {user: {name: "rails tutorial", email: "example@railstutorial.org",
                     password: "Password", password_confirmation: "Password"}}
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    log_in_as(user)
+    assert_not is_logged_in?
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: "wrong")
+    assert_not is_logged_in?
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.empty?
