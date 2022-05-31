d8961995abbdfed7fafa3e1c303db129172d91be
SeseriSalt <sho8.kthy@gmail.com>
2022-05-31 09:44:55 +0000

    Add user microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,10 @@ User.create!(name:  "Example User",
                password_confirmation: password,
                activated: true, activated_at: Time.zone.now)
 end
+
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Kpop.iii_groups
+  users.each { |user| user.microposts.create!(content: content) }
+end
