0ce47f676834ac340ca175b4d3d4ba2865b5d191
SeseriSalt <sho8.kthy@gmail.com>
2022-06-01 06:31:08 +0000

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,9 +18,17 @@ User.create!(name:  "Example User",
                activated: true, activated_at: Time.zone.now)
 end
 
-
+#microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Kpop.iii_groups
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+#relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
